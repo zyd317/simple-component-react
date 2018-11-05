290ad6c30d4cb88609019fae60236425341f0c3b
yidi.zhao <1005973585@qq.com>
2018-11-05 17:35:32 +0800

    change test path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: {
         index: path.join(__dirname, './test/index.js'), // index入口
-        lib: path.join(__dirname, './src/lib.js') // 单独打包库
+        lib: path.join(__dirname, './test/lib.js') // 单独打包库
     },
     output: {
         path: path.join(__dirname, 'build'),
