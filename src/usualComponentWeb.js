ef444cf3cbe51ffc5083df7bdafc56384f690161
yidi.zhao <1005973585@qq.com>
2019-01-09 14:23:32 +0800

    add lib

--- src/usualComponentWeb.js
+++ src/usualComponentWeb.js
@@ -1,7 +1,7 @@
 /**
  * 打包的每个分类常用组件，可以按需加载
  */
-const COMPONENT = require('./CooperateComponent');
+import COMPONENT from './CooperateComponent';
 import Animation from './Animation';
 import PopAlert from './PopAlert';
 import ConfirmDialog from './ConfirmDialog';
