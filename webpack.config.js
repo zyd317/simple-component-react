28de44207de0ebbdbd3948495eee29a60f5dd2e6
zhaoyidi <zhaoyidi@bytedance.com>
2019-04-10 15:31:05 +0800

    fix不支持flex

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,100 @@
  */
 let path = require('path');
 const autoprefixer = require('autoprefixer');
-module.exports = {
-    mode: 'production',
-    entry: {
-        index: path.join(__dirname, './src/index.js'),
-        usualComponentWeb: path.join(__dirname, './src/usualComponentWeb.js'),
-        usualComponentTouch: path.join(__dirname, './src/usualComponentTouch.js'),
-    },
-    output: {
-        path: path.join(__dirname, 'lib'),
-        filename: '[name].js',
-        publicPath: '/lib/',
-        library: 'SimpleComponent',
-        libraryTarget: "umd",
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    module: {
-        rules:[
-            {
-                test: /\.js[x]?$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0'
-                }]
-            },
-            {
-                test: /\.[s]?css$/,
-                use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader'
-                }, {
-                    loader: 'sass-loader'
-                }, {
-                    loader: 'postcss-loader',
-                    options: {
-                        ident: 'postcss',
-                        plugins: () => [
-                            autoprefixer({
-                                browsers: [
-                                    '>1%',
-                                    'last 4 versions',
-                                    'Firefox ESR',
-                                    'not ie < 9', // React doesn't support IE8 anyway
-                                ],
-                                flexbox: 'no-2009',
-                                remove: false,
-                            }),
-                        ],
-                    },
-                }]
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = (env, argv) => {
+    const commonConfig = {
+        mode: argv.mode,
+        entry: {
+            index: path.join(__dirname, './src/index.js'),
+            usualComponentWeb: path.join(__dirname, './src/usualComponentWeb.js'),
+            usualComponentTouch: path.join(__dirname, './src/usualComponentTouch.js'),
+        },
+        output: {
+            path: path.join(__dirname, 'lib'),
+            filename: '[name].js',
+            publicPath: '/lib/',
+            library: 'SimpleComponent',
+            libraryTarget: "umd",
+        },
+        resolve: {
+            extensions: [".js", ".jsx"]
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/](react-dom|react|addStyles|style-loader|scheduler)[\\/]/,
+                        name: 'vendor',
+                        chunks: 'all'
+                    }
+                }
             }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js[x]?$/,
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0'
+                    }]
+                },
+                {
+                    test: /\.[s]?css$/,
+                    use: [{
+                        loader: 'style-loader'
+                    }, {
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'sass-loader'
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                autoprefixer({
+                                    browsers: [
+                                        '>1%',
+                                        'last 4 versions',
+                                        'Firefox ESR',
+                                        'not ie < 9', // React doesn't support IE8 anyway
+                                    ],
+                                    flexbox: 'no-2009',
+                                    remove: false,
+                                }),
+                            ],
+                        },
+                    }]
+                }
+            ]
+        },
+        plugins: [
+            new BundleAnalyzerPlugin()
         ]
+    };
+
+    const isProdMode = argv.mode !== 'production';
+    if(isProdMode){
+        commonConfig.entry = {
+            index: path.join(__dirname, './src/index.js'),
+            usualComponentWeb: path.join(__dirname, './src/usualComponentWeb.js'),
+            usualComponentTouch: path.join(__dirname, './src/usualComponentTouch.js'),
+            indexTest: path.join(__dirname, './test/index.test.js'),
+            indexTest2: path.join(__dirname, './test/index.test2.js'),
+            indexTestTouch: path.join(__dirname, './test/index.test.touch.js'),
+        };
+        commonConfig.output = {
+            path: path.join(__dirname, 'dist'),
+            filename: '[name].js',
+            publicPath: '/dist/',
+            library: 'SimpleComponent',
+            libraryTarget: "umd",
+        };
+        commonConfig.devServer = {
+            host: '127.0.0.1',
+            port:8088
+        };
     }
+    return commonConfig;
 };
